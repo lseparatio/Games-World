026c590237c3378dc4475fde457ee762dba068f5
lseparatio <consultantaanglia@gmail.com>
2022-01-08 16:42:12 +0000

    Added functions to select a name store it and to change to categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 document.getElementById('submitButton').addEventListener('click', function() {
 	let userName = document.getElementById('inputName').value;
-  changeMiddleText();
+	if (userName == ""){
+		document.getElementById('tellMeYourName').innerHTML = 
+		`I am not allowed to play with strangers so please tell me your name!`
+	} else {
 	changeTopText(userName);
+  changeMiddleText();
+	chooseQuizCategory()
+	}
 });
 
 function changeTopText(userName) {
@@ -9,8 +15,11 @@ function changeTopText(userName) {
 		`<h1>Nice to meet you ${userName}!</h1>`;
 }
 
-
 function changeMiddleText() {
 	document.getElementById("welcomeMiddle").innerHTML =
 		`<h2>Do you think that you are ready to test your knowledge? Choose a category!</h2>`;
+}
+
+function chooseQuizCategory() {
+document.getElementById('gameContainer').innerHTML = "Choose categoryes html go here"
 }
