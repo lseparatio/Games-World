20f1a812721573ec96239e53107f388126dfc641
lseparatio <consultantaanglia@gmail.com>
2022-01-10 08:54:00 +0000

    Added cards for ability to choose game category

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,63 @@
-document.getElementById('submitButton').addEventListener('click', function() {
-	let userName = document.getElementById('inputName').value;
-	if (userName == ""){
-		document.getElementById('tellMeYourName').innerHTML = 
-		`I am not allowed to play with strangers so please tell me your name!`
+document.getElementById("submitButton").addEventListener("click", function () {
+	let userName = document.getElementById("inputName").value;
+	if (userName == "") {
+		document.getElementById(
+			"tellMeYourName"
+		).innerHTML = `I am not allowed to play with strangers so please tell me your name!`;
 	} else {
-	changeTopText(userName);
-  changeMiddleText();
-	chooseQuizCategory()
+		changeTopText(userName);
+		changeMiddleText();
+		chooseQuizCategory();
 	}
 });
 
 function changeTopText(userName) {
-	document.getElementById("welcomeTop").innerHTML =
-		`<h1>Nice to meet you ${userName}!</h1>`;
+	document.getElementById(
+		"welcomeTop"
+	).innerHTML = `<h1>Nice to meet you ${userName}!</h1>`;
 }
 
 function changeMiddleText() {
-	document.getElementById("welcomeMiddle").innerHTML =
-		`<h2>Do you think that you are ready to test your knowledge? Choose a category!</h2>`;
+	document.getElementById(
+		"welcomeMiddle"
+	).innerHTML = `<h2>Do you think that you are ready to test your knowledge? Choose a category!</h2>`;
 }
 
 function chooseQuizCategory() {
-document.getElementById('gameContainer').innerHTML = "Choose categoryes html go here"
+	document.getElementById(
+		"gameContainer"
+	).innerHTML = `
+	<div class="row" id="game">
+<div class="col d-flex">
+<div class="card" style="width: 18rem;">
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+	<h5 class="card-title">Card title</h5>
+	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div>
+<div class="col d-flex">
+<div class="card" style="width: 18rem;">
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+	<h5 class="card-title">Card title</h5>
+	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div>
+<div class="col d-flex">
+<div class="card" style="width: 18rem;">
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+	<h5 class="card-title">Card title</h5>
+	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div>
+</div>
+`;
 }
